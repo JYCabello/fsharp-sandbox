f01a466e2b9f17e8fa76c0f67e11b565b17b9f8c
JYCabello <j.cabello.ramos@gmail.com>
2021-01-03 12:55:29 +0100

    Fiddling with correcursion

--- FSharpSandbox/Program.fs
+++ FSharpSandbox/Program.fs
@@ -2,7 +2,24 @@
 
 open System
 
+let rec mapStackOverflow f = function
+    | [] -> []
+    | x::xs -> f x::mapStackOverflow f xs
+
+let mapTailRecAcc fn lst =
+    let rec loop acc = function
+        | []    -> acc |> List.rev
+        | x::xs -> loop (fn x :: acc) xs
+    loop lst []
+
+let map f l =
+    let rec loop cont = function
+        | [] -> cont []
+        | x::xs -> loop ( fun acc -> cont (f x::acc) ) xs
+    loop id l
+
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    let meh = [1..10_000_000] |> mapTailRecAcc ((+) 1)
+    printfn "%i" meh.Length
+    0
